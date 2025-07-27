82984ecd6315447b50177da43e687b654b832c1a
ajiadiyudha <adiyudha@cloudflare.com>
2025-07-27 14:49:40 +0800

    initial commit for 40net demo test

--- gcp/7.6/single/main.tf
+++ gcp/7.6/single/main.tf
@@ -1,162 +1,261 @@
-### GCP terraform
 terraform {
-  required_version = ">=0.12.0"
+  required_version = ">= 1.0"
   required_providers {
-    google      = ">=2.11.0"
-    google-beta = ">=2.13"
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.50.0"
+    }
   }
 }
+
 provider "google" {
-  project      = var.project
-  region       = var.region
-  zone         = var.zone
-  access_token = var.token
-}
-provider "google-beta" {
-  project      = var.project
-  region       = var.region
-  zone         = var.zone
-  access_token = var.token
+  project = var.project
+  region  = "us-central1"
+  zone    = "us-central1-a"
 }
 
-resource "google_compute_image" "fgtvmgvnic" {
-  count = var.nictype == "GVNIC" ? 1 : 0
-  name  = "fgtvmgvnic-image"
+# --- Variables ---
+#variable "project" {
+#  description = "Your GCP Project ID."
+#  type        = string
+#}
 
-  source_image = var.image
-
-  guest_os_features {
-    type = var.nictype
-  }
+variable "fortigate_password" {
+  description = "A secure password for the FortiGate admin user."
+  type        = string
+  sensitive   = true
 }
 
-# Randomize string to avoid duplication
-resource "random_string" "random_name_post" {
-  length           = 3
-  special          = true
-  override_special = ""
-  min_lower        = 3
+variable "linux_password" {
+  description = "A secure password for the tfadmin user on the Linux VMs."
+  type        = string
+  sensitive   = true
 }
 
-# Create log disk
-resource "google_compute_disk" "logdisk" {
-  name = "log-disk-${random_string.random_name_post.result}"
-  size = 30
-  type = "pd-standard"
-  zone = var.zone
-}
-
-
-### VPC ###
-resource "google_compute_network" "vpc_network" {
-  name                    = "vpc-public-${random_string.random_name_post.result}"
+# --- Virtual Networks ---
+# VPC for the FortiGate's external interface
+resource "google_compute_network" "external_vpc" {
+  name                    = "external-vpc"
   auto_create_subnetworks = false
+  routing_mode            = "REGIONAL"
 }
 
-resource "google_compute_network" "vpc_network2" {
-  name                    = "vpc2-private-${random_string.random_name_post.result}"
+# VPC for all internal resources
+resource "google_compute_network" "seahk_pl_interview_vnet" {
+  name                    = "seahk-pl-interview-vnet"
   auto_create_subnetworks = false
+  routing_mode            = "REGIONAL"
 }
 
-
-### Public Subnet ###
-resource "google_compute_subnetwork" "public_subnet" {
-  name                     = "public-subnet-${random_string.random_name_post.result}"
-  region                   = var.region
-  network                  = google_compute_network.vpc_network.name
-  ip_cidr_range            = var.public_subnet
-  private_ip_google_access = true
-}
-### Private Subnet ###
-resource "google_compute_subnetwork" "private_subnet" {
-  name          = "private-subnet-${random_string.random_name_post.result}"
-  region        = var.region
-  network       = google_compute_network.vpc_network2.name
-  ip_cidr_range = var.protected_subnet
+# --- Subnets ---
+resource "google_compute_subnetwork" "out_subnet" {
+  name          = "seahk-pl-interview-vnet-out"
+  region        = "us-central1"
+  network       = google_compute_network.external_vpc.id # Attached to external_vpc
+  ip_cidr_range = "172.37.0.0/24"
 }
 
-# Firewall Rule External
-resource "google_compute_firewall" "allow-fgt" {
-  name    = "allow-fgt-${random_string.random_name_post.result}"
-  network = google_compute_network.vpc_network.name
+resource "google_compute_subnetwork" "in_subnet" {
+  name          = "seahk-pl-interview-vnet-in"
+  region        = "us-central1"
+  network       = google_compute_network.seahk_pl_interview_vnet.id # Attached to internal vnet
+  ip_cidr_range = "172.37.1.0/24"
+}
 
+resource "google_compute_subnetwork" "web_subnet" {
+  name          = "seahk-pl-interview-vnet-web"
+  region        = "us-central1"
+  network       = google_compute_network.seahk_pl_interview_vnet.id # Attached to internal vnet
+  ip_cidr_range = "172.37.2.0/24"
+}
+
+resource "google_compute_subnetwork" "app_subnet" {
+  name          = "seahk-pl-interview-vnet-app"
+  region        = "us-central1"
+  network       = google_compute_network.seahk_pl_interview_vnet.id # Attached to internal vnet
+  ip_cidr_range = "172.37.3.0/24"
+}
+
+# --- GCP Firewall Rule to allow traffic TO the FortiGate ---
+resource "google_compute_firewall" "allow_inbound_to_fgt" {
+  name    = "allow-inbound-to-fgt"
+  network = google_compute_network.external_vpc.id # Rule applies to the external_vpc
   allow {
     protocol = "tcp"
-    ports    = ["22", "80", "443"]
+    ports    = ["443", "1022", "1023"]
+  }
+  allow { protocol = "icmp" }
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["fortigate"]
+}
+
+# --- Provision FortiGate (PAYG) ---
+resource "google_compute_instance" "fortigate_vm" {
+  name           = "fortigate-vm"
+  machine_type   = "n1-standard-1"
+  tags           = ["fortigate"]
+  can_ip_forward = true
+
+  boot_disk {
+    initialize_params {
+      image = "projects/fortigcp-project-001/global/images/fortinet-fgtondemand-763-20250423-001-w-license"
+      size  = 30
+    }
   }
 
+  # NIC 0 attached to the external VPC
+  network_interface {
+    subnetwork = google_compute_subnetwork.out_subnet.id
+    network_ip = "172.37.0.4"
+    access_config {} # Binds the Public IP
+  }
+
+  # NIC 1 attached to the internal VPC
+  network_interface {
+    subnetwork = google_compute_subnetwork.in_subnet.id
+    network_ip = "172.37.1.4"
+  }
+
+  metadata = {
+    user-data = templatefile("${path.module}/fortigate_config.txt", { fgt_password = var.fortigate_password })
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+# --- Provision Linux Web Server ---
+resource "google_compute_instance" "web_server" {
+  name         = "linux-web-server"
+  machine_type = "e2-small"
+  tags         = ["web"]
+
+  boot_disk {
+    initialize_params {
+      image = "projects/debian-cloud/global/images/family/debian-12"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.web_subnet.id
+    network_ip = "172.37.2.10"
+  }
+
+  metadata = {
+    startup-script = <<-EOF
+      #!/bin/bash
+      # Install necessary packages
+      apt-get update
+      apt-get install -y docker.io openssh-server
+
+      # Start and enable Docker
+      systemctl start docker
+      systemctl enable docker
+      docker run --name web-container -d -p 80:80 nginx
+
+      # Create user and set password
+      useradd -m -s /bin/bash tfadmin
+      echo "tfadmin:${var.linux_password}" | chpasswd
+      usermod -aG docker tfadmin
+
+      # Configure SSH to allow password authentication
+      # Use sed to replace the line directly for reliability
+      sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+      sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+      # Restart SSH service to apply changes
+      systemctl restart sshd
+    EOF
+  }
+}
+
+# --- Requirement 3.7 & Docker: Provision Linux App Server ---
+resource "google_compute_instance" "app_server" {
+  name         = "linux-app-server"
+  machine_type = "e2-small"
+  tags         = ["app"]
+
+  boot_disk {
+    initialize_params {
+      # Using a standard Debian 12 image
+      image = "projects/debian-cloud/global/images/family/debian-12"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.app_subnet.id
+    network_ip = "172.37.3.10"
+  }
+
+  metadata = {
+    startup-script = <<-EOF
+      #!/bin/bash
+      # Install necessary packages
+      apt-get update
+      apt-get install -y docker.io openssh-server
+
+      # Start and enable Docker
+      systemctl start docker
+      systemctl enable docker
+      docker run --name app-container -d -p 8080:80 httpd
+
+      # Create user and set password
+      useradd -m -s /bin/bash tfadmin
+      echo "tfadmin:${var.linux_password}" | chpasswd
+      usermod -aG docker tfadmin
+
+      # Configure SSH to allow password authentication
+      # Use sed to replace the line directly for reliability
+      sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+      sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+      # Restart SSH service to apply changes
+      systemctl restart sshd
+    EOF
+  }
+}
+
+# --- GCP Firewall Rule for Internal Communication ---
+resource "google_compute_firewall" "allow_internal_traffic" {
+  name    = "allow-internal-all"
+  network = google_compute_network.seahk_pl_interview_vnet.id
+
+  # Allow all protocols on all ports
+  allow {
+    protocol = "tcp"
+    ports    = ["0-65535"]
+  }
+  allow {
+    protocol = "udp"
+    ports    = ["0-65535"]
+  }
   allow {
     protocol = "icmp"
   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["allow-fgt"]
+  # Apply this rule to traffic coming from any internal IP in the VPC
+  source_ranges = ["172.37.0.0/16"]
+
+  # This rule applies TO VMs with these tags
+  target_tags = ["fortigate", "web", "app"]
 }
 
-# Firewall Rule Internal
-resource "google_compute_firewall" "allow-internal" {
-  name    = "allow-internal-${random_string.random_name_post.result}"
-  network = google_compute_network.vpc_network2.name
-
-  allow {
-    protocol = "all"
-  }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["allow-internal"]
-}
-
-
-# Create FGTVM compute instance
-resource "google_compute_instance" "default" {
-  name           = "fgtnat-${random_string.random_name_post.result}"
-  machine_type   = var.machine
-  zone           = var.zone
-  can_ip_forward = "true"
-
-  tags = ["allow-fgt", "allow-internal"]
-
-  boot_disk {
-    initialize_params {
-      image = var.nictype == "GVNIC" ? google_compute_image.fgtvmgvnic[0].self_link : var.image
-    }
-  }
-  attached_disk {
-    source = google_compute_disk.logdisk.name
-  }
-  network_interface {
-    subnetwork = google_compute_subnetwork.public_subnet.name
-    nic_type   = var.nictype
-    access_config {
-    }
-  }
-  network_interface {
-    subnetwork = google_compute_subnetwork.private_subnet.name
-    nic_type   = var.nictype
-  }
-  metadata = {
-    user-data = fileexists("${path.module}/${var.user_data}") ? "${file(var.user_data)}" : null
-    license   = fileexists("${path.module}/${var.license_file}") ? "${file(var.license_file)}" : null
-  }
-  service_account {
-    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
-  }
-  scheduling {
-    preemptible       = false
-    automatic_restart = false
-  }
-}
-
-data "google_compute_instance" "default" {
-  depends_on = [google_compute_instance.default]
-  name       = "fgtnat-${random_string.random_name_post.result}"
-}
-
-resource "google_compute_route" "internal" {
-  name        = "internal-route-${random_string.random_name_post.result}"
+# --- Create Routing in the internal VPC ---
+resource "google_compute_route" "web_to_fgt_route" {
+  name        = "route-web-to-fgt"
   dest_range  = "0.0.0.0/0"
-  network     = google_compute_network.vpc_network2.name
-  next_hop_ip = data.google_compute_instance.default.network_interface.1.network_ip
+  network     = google_compute_network.seahk_pl_interview_vnet.id
+  next_hop_ip = google_compute_instance.fortigate_vm.network_interface[1].network_ip
   priority    = 100
-  depends_on  = [google_compute_instance.default]
+  tags        = ["web"]
 }
 
+resource "google_compute_route" "app_to_fgt_route" {
+  name        = "route-app-to-fgt"
+  dest_range  = "0.0.0.0/0"
+  network     = google_compute_network.seahk_pl_interview_vnet.id
+  next_hop_ip = google_compute_instance.fortigate_vm.network_interface[1].network_ip
+  priority    = 100
+  tags        = ["app"]
+}
